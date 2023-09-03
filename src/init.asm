f7b189f0a7e14ef9692737fe16d3b62cc155fa59
vladikcomper <rubstov@gmail.com>
2023-09-03 02:02:04 +0200

    Enable DAC and setup panning during initialization.

--- src/init.asm
+++ src/init.asm
@@ -13,10 +13,20 @@ InitDriver:
 	; Initialize stack
 	ld	sp, Stack
 
+	; Set YM for DAC playback
+	ld	hl, YM_Port0_Reg
+	ld	(hl), 2Bh		; YM => Send 2B 80 (enable DAC)
+	inc	l			; ''
+	ld	(hl), 80h		; ''
+	inc	l			; ''
+	ld	(hl), 0B6h		; YM => Send B6 C0 (set DAC panning to LR)
+	inc	l			; ''
+	ld	(hl), 0C0h		; ''
+
 	; Initialize index registers
 	ld	iy, DriverIO_RAM
 
 	; Mark driver as ready for operation
-	ld	(iy+sDriverIO.OUT_ready), 01h	
+	ld	(iy+sDriverIO.OUT_ready), 01h
 
 	jr	IdleLoop_Init
