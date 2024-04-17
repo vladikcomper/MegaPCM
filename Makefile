b015337d9f3c07b4dd807795ce2dec1e23ae2090
vladikcomper <rubstov@gmail.com>
2024-04-17 03:52:24 +0200

    Build system: Windows support [1]

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
 
 .PHONY:	megapcm examples test clean
 
+MAKEFILE := Makefile
+ifeq ($(OS),Windows_NT)
+	MAKEFILE := Makefile.win
+endif
+
 megapcm:
-	$(MAKE) -C src
+	$(MAKE) -C src -f $(MAKEFILE)
 
 examples:	megapcm
-	$(MAKE) -C examples
+	$(MAKE) -C examples -f $(MAKEFILE)
 
 test:	megapcm
-	$(MAKE) -C test
+	$(MAKE) -C test -f $(MAKEFILE)
 
 clean:
-	$(MAKE) -C src clean
-	$(MAKE) -C examples clean
-	$(MAKE) -C test clean
+	$(MAKE) -C src -f $(MAKEFILE) clean
+	$(MAKE) -C examples -f $(MAKEFILE) clean
+	$(MAKE) -C test -f $(MAKEFILE) clean
--- toolchain.mk
+++ toolchain.mk
@@ -2,27 +2,33 @@
 # This Makfile shouldn't be invoked directly!
 
 ifndef TOOLCHAIN_DIR
-$(error TOOLCHAIN_DIR wasn't specified)
+$(error TOOLCHAIN_DIR wasn't specified. Don't invoke this file directly!)
 endif
 
-# Common python tools
-BLOBTOASM := python $(TOOLCHAIN_DIR)/blobtoasm.py
-SYMTOH := python $(TOOLCHAIN_DIR)/symtoh.py
-MKVOLUME := python $(TOOLCHAIN_DIR)/mkvolume.py
-MKDPCMTBL := python $(TOOLCHAIN_DIR)/mkdpcmtbl.py
+# Identify Python 3 installation
+ifndef PYTHON
+ifeq ($(shell python -c "import sys; print(sys.version_info[0])"),3)
+	PYTHON := python
+else ifeq ($(shell python3 -c "import sys; print(sys.version_info[0])"),3)
+	PYTHON := python3
+else
+$(error Couldn't find Python 3 in your system. Please install it or manually specify PYTHON variable)
+endif
+endif
 
 # OS-specific tools
 ifeq ($(OS),Windows_NT)
 	# Windows commands
-	SJASMPLUS := $(TOOLCHAIN_DIR)/sjasmplus.exe
-	ASM68K := $(TOOLCHAIN_DIR)/asm68k.exe
-	PSYLINK := $(TOOLCHAIN_DIR)/psylink.exe
-	CONVSYM := $(TOOLCHAIN_DIR)/convsym.exe
-	CBUNDLE := $(TOOLCHAIN_DIR)/cbundle.exe
+	SJASMPLUS := $(TOOLCHAIN_DIR)\\sjasmplus.exe
+	ASM68K := $(TOOLCHAIN_DIR)\\asm68k.exe
+	PSYLINK := $(TOOLCHAIN_DIR)\\psylink.exe
+	CONVSYM := $(TOOLCHAIN_DIR)\\convsym.exe
+	CBUNDLE := $(TOOLCHAIN_DIR)\\cbundle.exe
+	BLOBTOASM := $(PYTHON) $(TOOLCHAIN_DIR)\\blobtoasm.py
+	SYMTOH := $(PYTHON) $(TOOLCHAIN_DIR)\\symtoh.py
+	MKVOLUME := $(PYTHON) $(TOOLCHAIN_DIR)\\mkvolume.py
+	MKDPCMTBL := $(PYTHON) $(TOOLCHAIN_DIR)\\mkdpcmtbl.py
 	CC := gcc
-	CP := copy
-	MKDIR := -md
-	RM := del
 
 else
 	# Unix commands
@@ -31,13 +37,14 @@ else
 	PSYLINK := wine $(TOOLCHAIN_DIR)/psylink.exe
 	CONVSYM := convsym
 	CBUNDLE := cbundle
+	BLOBTOASM := $(PYTHON) $(TOOLCHAIN_DIR)/blobtoasm.py
+	SYMTOH := $(PYTHON) $(TOOLCHAIN_DIR)/symtoh.py
+	MKVOLUME := $(PYTHON) $(TOOLCHAIN_DIR)/mkvolume.py
+	MKDPCMTBL := $(PYTHON) $(TOOLCHAIN_DIR)/mkdpcmtbl.py
 	CC := cc
-	CP := cp
-	MKDIR := mkdir -p
-	RM := rm -rf
 
 	# Fallback to Wine if some tools aren't installed
-	ifeq (,$(shell which %$(SJASMPLUS)))
+	ifeq (,$(shell which $(SJASMPLUS)))
 		SJASMPLUS := wine $(TOOLCHAIN_DIR)/sjasmplus.exe
 	endif
 	ifeq (,$(shell which $(CONVSYM)))
