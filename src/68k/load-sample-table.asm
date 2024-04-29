vladikcomper/MegaPCM

commit 664dcb55b7fad52cd6940e8828be1ebc4755e3d8

68k library: Disable interrupts in LoadDriver and LoadSampleTable.
This is to be expected, but let's force-disable interrupts just in case for stability's sake.

src/68k/load-sample-table.asm
@@ -172,6 +172,8 @@ MegaPCM_LoadSampleTable:
 		swap	@var1								; @var1 HIGH = end bank
 
 		; We can send processed data to Mega PCM's sample table now ...
+		move.w	sr, -(sp)
+		move.w	#$2700, sr							; disable interrupts
 		stopZ80	(@z80_busreq)
 		move.b	@sample_type, (@z80_sample_tbl)+	; 00h	- sample type
 		move.b	@sample_flags, (@z80_sample_tbl)+	; 01h	- sample flags
@@ -183,6 +185,7 @@ MegaPCM_LoadSampleTable:
 		move.b	3(sp), (@z80_sample_tbl)+			; 07h	- end offset LOW
 		move.b	2(sp), (@z80_sample_tbl)+			; 08h	- end offset HIGH
 		startZ80 (@z80_busreq)
+		move.w	(sp)+, sr							; restore interrupts		
 
 		dbf		@sample_cnt, @ProcessSampleLoop
 		bra.s	@Err_TooManySamples
@@ -201,11 +204,14 @@ MegaPCM_LoadSampleTable:
 	; ----------------------------------------------------------------------
 	@WriteEmptyRecord:
 		KDebug.WriteLine "Sample: <None>"
+		move.w	sr, -(sp)
+		move.w	#$2700, sr							; disable interrupts
 		stopZ80	(@z80_busreq)
 		rept 9
 			move.b	@sample_type, (@z80_sample_tbl)+
 		endr
 		startZ80 (@z80_busreq)
+		move.w	(sp)+, sr							; restore interrupts
 
 		lea		11(@sample_tbl), @sample_tbl		; skip the remaining bytes
 		dbf		@sample_cnt, @ProcessSampleLoop

src/68k/load-driver.asm
@@ -6,6 +6,9 @@
 
 MegaPCM_LoadDriver:
 
+	move.w	sr, -(sp)
+	move.w	#$2700, sr					; disable interrupts
+
 	; ----------------------------------------------------------------------
 	; USES:
 	@var0:			equr	d0			;		generic variable #1
@@ -71,6 +74,7 @@ MegaPCM_LoadDriver:
 
 ; ----------------------------------------------------------------------
 @Done:
-	; Release additional registers and quit
+	; Release additional registers, restore SR and quit
 	move.l	(sp)+, a3
+	move.w	(sp)+, sr
 	rts

commit c033e317186746fe37a1d9cb9f509769278ef8af

68k library: Hotfix broken Z80 offsets upon sample table upload.
A bug introduced in 664dcb55b7fad52cd6940e8828be1ebc4755e3d8.

src/68k/load-sample-table.asm
@@ -180,10 +180,10 @@ MegaPCM_LoadSampleTable:
 		move.b	@sample_pitch, (@z80_sample_tbl)+	; 02h	- pitch
 		move.b	@var0, (@z80_sample_tbl)+			; 03h	- start bank
 		move.b	@var1, (@z80_sample_tbl)+			; 04h	- end bank
-		move.b	1(sp), (@z80_sample_tbl)+			; 05h	- start offset LOW
-		move.b	(sp), (@z80_sample_tbl)+			; 06h	- start offset HIGH
-		move.b	3(sp), (@z80_sample_tbl)+			; 07h	- end offset LOW
-		move.b	2(sp), (@z80_sample_tbl)+			; 08h	- end offset HIGH
+		move.b	2+1(sp), (@z80_sample_tbl)+			; 05h	- start offset LOW
+		move.b	2+0(sp), (@z80_sample_tbl)+			; 06h	- start offset HIGH
+		move.b	2+3(sp), (@z80_sample_tbl)+			; 07h	- end offset LOW
+		move.b	2+2(sp), (@z80_sample_tbl)+			; 08h	- end offset HIGH
 		startZ80 (@z80_busreq)
 		move.w	(sp)+, sr							; restore interrupts		
 