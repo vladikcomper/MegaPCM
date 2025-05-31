4938ff42d9522406a2c7b208a4f9324d7cf36f46
vladikcomper <rubstov@gmail.com>
2025-05-31 22:25:57 +0200

    Tools: `dpcm-hq-conv`: Update reference implementation.

--- tools/dpcm-hq-conv.py
+++ tools/dpcm-hq-conv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from typing import Tuple
+from typing import Callable, Tuple
+from itertools import product
 import argparse
 import numpy as np
 import math
@@ -16,10 +17,10 @@ deltaTables = (
 )
 
 
-input_buff = np.fromfile(input_fn, dtype=np.int8) + (-128)
-
-def encode(samples: np.ndarray, deltaTable: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-	if samples.size % 2: raise Exception('Buffer should contain even number of samples')
+def encodeV1(samples: np.ndarray, deltaTable: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+	if samples.size % 2: raise Exception('Buffer must contain even number of samples')
+	if samples.dtype != np.uint8: raise Exception('Buffer must be 8-bit unsigned PCM')
+	samples = samples.astype(np.int8) + (-128)
 	samples_predicted = np.empty_like(samples)
 	outdeltas = np.empty_like(samples)
 	current_sample = 0
@@ -32,8 +33,54 @@ def encode(samples: np.ndarray, deltaTable: np.ndarray) -> Tuple[np.ndarray, np.
 		outdeltas[i] = best_delta_index
 		samples_predicted[i] = current_sample
 
-	rmse = np.sqrt(np.mean((samples_predicted - samples) ** 2))
+	rmse = np.sqrt(np.mean(np.subtract(samples_predicted, samples, dtype=np.int16) ** 2))
 	return ((outdeltas[::2] << 4) + outdeltas[1::2], rmse)
+
+
+def encodeV1B(samples: np.ndarray, deltaTable: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+	if samples.size % 2: raise Exception('Buffer must contain even number of samples')
+	if samples.dtype != np.uint8: raise Exception('Buffer must be 8-bit unsigned PCM')
+	samples = samples.astype(np.int8) + (-128)
+	samples_predicted = np.empty_like(samples)
+	outdeltas = np.empty_like(samples)
+	current_sample = 0
+
+	for i in range(len(samples)):
+		actual_sample = samples[i]
+		predicted_samples = current_sample + deltaTable
+		errors = np.abs(np.subtract(actual_sample, predicted_samples, dtype=np.int16))
+		best_delta_indexes = np.flatnonzero(errors == errors.min())
+		best_delta_index = best_delta_indexes[-1]
+		current_sample = predicted_samples[best_delta_index]
+		outdeltas[i] = best_delta_index
+		samples_predicted[i] = current_sample
+
+	rmse = np.sqrt(np.mean(np.subtract(samples_predicted, samples, dtype=np.int16) ** 2))
+	return ((outdeltas[::2] << 4) + outdeltas[1::2], rmse)
+
+
+def encodeV2(samples: np.ndarray, delta_table: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+	if samples.size % 2: raise Exception('Buffer should contain even number of samples')
+	if samples.dtype != np.uint8: raise Exception('Buffer must be 8-bit unsigned PCM')
+	samples_predicted = np.empty_like(samples)
+	outdeltas = np.empty_like(samples)
+	current_sample = 0x80
+
+	for i in range(len(samples)):
+		actual_sample = samples[i]
+		predicted_samples = np.add(current_sample, delta_table, dtype=np.int16)
+		nonclipped_predicted_samples = predicted_samples.astype(np.uint16) < 0x100
+		abs_errors = np.full_like(predicted_samples, 0x7FFF)
+		np.abs(actual_sample - predicted_samples, out=abs_errors, where=nonclipped_predicted_samples) # calc errors for non-clipped samples only
+		best_delta_index = np.argmin(abs_errors)
+		current_sample = predicted_samples[best_delta_index]
+		outdeltas[i] = best_delta_index
+		samples_predicted[i] = current_sample
+
+	rmse = np.sqrt(np.mean(np.subtract(samples_predicted, samples, dtype=np.int16) ** 2))
+	return ((outdeltas[::2] << 4) + outdeltas[1::2], rmse)
+
+encode_algorithms = (encodeV1, encodeV1B, encodeV2)
 
 
 def decode(deltaNibbles: np.ndarray, deltaTable: np.ndarray):
@@ -41,7 +88,15 @@ def decode(deltaNibbles: np.ndarray, deltaTable: np.ndarray):
 	deltasBytes[0::2] = deltaNibbles >> 4
 	deltasBytes[1::2] = deltaNibbles & 0xF
 	deltas = deltaTable[deltasBytes]
-	return deltas.cumsum(dtype=np.int8)
+	return (deltas.cumsum(dtype=np.int8) + (-128)).astype(np.uint8)
+
+
+def runDecoder(path: str) -> np.ndarray:
+	input_buff = np.fromfile(path, dtype=np.uint8)
+	if input_buff[0] != 0xD0:
+		return decode(input_buff, deltaTables[0])
+	else:
+		return decode(input_buff[2:], deltaTables[int(input_buff[1])])
 
 
 if __name__ == '__main__':
@@ -49,24 +104,56 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='DPCM-HQ compressor and decompressor (reference implementation)')
 	parser.add_argument("input_filename", help="Input file path (DPCM or raw PCM)")
 	parser.add_argument("output_filename", help="Output file path (raw PCM or DPCM)")
+	parser.add_argument("-a", "--algorithm", type=int, default=-1)
+	parser.add_argument("-t", "--table", type=int, default=-1)
 	parser.add_argument("-d", "--decompress", action="store_true")
-	parser.add_argument("-t", "--table", type=int, default=0)
+	parser.add_argument("-l", "--logdeltas", action="store_true")
+	parser.add_argument("-c", "--compare", action="store_true")
 	args = parser.parse_args()
 
+	# Comparison mode
+	if args.compare:
+		print(f"Comparing '{args.input_filename}' (PCM source) with '{args.output_filename}' (DPCM)...")
+		input_buff = np.fromfile(args.input_filename, dtype=np.uint8)
+		input_buff_2 = runDecoder(args.output_filename)
+		if input_buff.size != input_buff_2.size: raise Exception(f"Number of samples don't match ({input_buff.size} != {input_buff_2.size})")
+		rmse = np.sqrt(np.mean(np.subtract(input_buff_2, input_buff, dtype=np.int16) ** 2))
+		print(f"rmse={rmse:f}")
+
 	# Default compression mode
-	if not args.decompress:
-		input_buff = np.fromfile(args.input_filename, dtype=np.int8) + (-128) # signed 8-bit format for faster overflow detection
-		delta_table = deltaTables[args.table]
-		output_buff, rmse = encode(input_buff, delta_table)
-		print(f"RMSE: {rmse:f}")
+	elif not args.decompress:
+		print(f"Compressing '{args.input_filename}' (PCM) into '{args.output_filename}' (DPCM)...")
+
+		input_buff = np.fromfile(args.input_filename, dtype=np.uint8)
+
+		algos = encode_algorithms if args.algorithm == -1 else (encode_algorithms[args.algorithm],)
+		tables = deltaTables if args.table == -1 else (deltaTables[args.table],)
+
+		best_rmse, best_rmse_index = 0x100, -1
+		results = []
+		for (algorithm, (table_index, delta_table)) in product(algos, enumerate(tables)):
+			print(f"#{len(results):d}: encoder={algorithm.__name__}, table={table_index:d}...")
+			output_buff, rmse = algorithm(input_buff, delta_table)
+			print(f"    rmse={rmse:f}")
+			results.append((output_buff, rmse, table_index))
+			if rmse < best_rmse:
+				best_rmse = rmse
+				best_rmse_index = len(results)-1
+
+		output_buff, rmse, table_index = results[best_rmse_index]
+		if len(results) > 1:
+			print(f"Selecting result #{best_rmse_index:d}")
+			print(f"rmse={rmse:f}")
+
 		with open(args.output_filename, 'wb') as output_file:
-			output_file.write(bytes([0xD0, args.table]))
+			output_file.write(bytes((0xD0,table_index)))
 			output_buff.tofile(output_file)
 
+	# Decompression mode
 	else:
-		input_buff = np.fromfile(args.input_filename, dtype=np.uint8)
-		if input_buff[0] != 0xD0:
-			raise Exception('Invalid DPCM-HQ header: 0xD0 expected as the first byte')
-		delta_table = deltaTables[int(input_buff[1])]
-		output_buff = decode(input_buff[2:], delta_table) + (-128)
-		output_buff.tofile(args.output_filename)
+		output_buff = runDecoder(args.input_filename)
+		if args.logdeltas:
+			output_deltas = np.diff(output_buff, prepend=0x80)
+			np.savetxt(args.output_filename, output_deltas, fmt="%d")
+		else:
+			output_buff.tofile(args.output_filename)
