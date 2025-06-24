2176280638fbabec326a4cdb3d67ef9fd4efe53b
vladikcomper <rubstov@gmail.com>
2025-06-24 09:22:48 +0200

    Test suite: Test DPCM playback in `sample-buffer-playback` tests.
    This commit also adds generic format support (currently just DPCM) in MegaPCM playback emulator.

--- test/sample-buffer-playback.c
+++ test/sample-buffer-playback.c
@@ -14,31 +14,36 @@
  */
 typedef struct {
 	uint8_t sampleType;
-	uint32_t offset;
+	uint32_t pos;
 	uint32_t length;
 	uint8_t pitch;
 	uint8_t pitchCounter;
+	const uint8_t *referenceSamples;
 } EmulatedPlaybackState;
 
-const uint8_t sample_2[]   = { 0xFF, 0xFF };
-const uint8_t sample_8[]   = { 0, 1, 2, 3, 4, 5, 6, 7 };
-const uint8_t sample_254[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
-							   21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
-							   39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 
-							   57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 
-							   75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 
-							   93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 
-							   109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 
-							   124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 
-							   139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 
-							   154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 
-							   169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 
-							   184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 
-							   199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 
-							   214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 
-							   229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 
-							   244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254 };
+const uint8_t sample_pcm_2[]   = { 0xFF, 0xFF };
+const uint8_t sample_pcm_8[]   = { 0, 1, 2, 3, 4, 5, 6, 7 };
+const uint8_t sample_pcm_254[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+								  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+								  39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 
+								  57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 
+								  75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 
+								  93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 
+								  109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 
+								  124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 
+								  139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 
+								  154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 
+								  169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 
+								  184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 
+								  199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 
+								  214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 
+								  229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 
+								  244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254 };
 
+const uint8_t sample_dpcm_4[] = { 0x01, 0x01 };
+const uint8_t sample_dpcm_4_output[] = { 0x80, 0x81, 0x81, 0x82 };
+const uint8_t sample_dpcm_8[] = { 0x01, 0x01, 0x09, 0x09 };
+const uint8_t sample_dpcm_8_output[] = { 0x80, 0x81, 0x81, 0x82, 0x82, 0x81, 0x81, 0x80 };
 
 /* Lightweight emulator of Mega PCM's playback routine to ensure driver operates correctly */
 // TODO: Move this to `megapcm-emu`
@@ -51,15 +56,19 @@ uint8_t emulateSamplePlayback(Z80VM_Context * context) {
 	}
 	if (playbackState->length == 0) {
 		fprintf(stderr, "Attempt to read past the end of sample\n");
+		Z80VM_DumpCPURegisters(context);
 		abort();
 	}
 
 	/* Fetch sample */
-	uint8_t sample = context->ROM[playbackState->offset];
+	uint8_t sample = playbackState->referenceSamples[playbackState->pos];
 
 	/* Apply pitch */
-	if ((playbackState->sampleType == Z_MPCM_TYPE_PCM_TURBO) || ((uint16_t)playbackState->pitchCounter + (uint16_t)playbackState->pitch >= 0x100)) {
-		playbackState->offset++;
+	if ((playbackState->sampleType == Z_MPCM_TYPE_PCM_TURBO) 
+			|| (playbackState->sampleType == Z_MPCM_TYPE_DPCM_TURBO)
+			|| ((uint16_t)playbackState->pitchCounter + (uint16_t)playbackState->pitch >= 0x100)
+	) {
+		playbackState->pos++;
 		playbackState->length--;
 	}
 	playbackState->pitchCounter += playbackState->pitch;
@@ -97,7 +106,14 @@ void runTest_WriteByteCallback(uint16_t address, uint8_t value, Z80VM_Context * 
 	}
 }
 
-void runTest(Z80VM_Context * context, uint8_t sampleType, const uint8_t * sample, size_t sampleSize, uint32_t startOffsetInROM) {
+void runTest(
+		Z80VM_Context * context,
+		uint8_t sampleType,
+		const uint8_t * sample,
+		const uint8_t * reference_sample,
+		size_t sampleSize,
+		uint32_t startOffsetInROM
+) {
 
 	fprintf(stderr, "Testing sample: %ld bytes, @%X...\n", sampleSize, startOffsetInROM);
 
@@ -126,10 +142,11 @@ void runTest(Z80VM_Context * context, uint8_t sampleType, const uint8_t * sample
 	/* Setup playback emulation state */
 	EmulatedPlaybackState playbackState = {
 		.sampleType = sampleType,
-		.offset = startOffsetInROM,
-		.length = sampleSize,
+		.pos = 0,
+		.length = sampleSize * (((sampleType == Z_MPCM_TYPE_DPCM) || (sampleType == Z_MPCM_TYPE_DPCM_TURBO)) ? 2 : 1),
 		.pitch = pitch,
-		.pitchCounter = 0
+		.pitchCounter = 0,
+		.referenceSamples = reference_sample,
 	};
 	context->stateExtension = &playbackState;
 
@@ -178,22 +195,26 @@ int main(int argc, char * argv[]) {
 	/* Run actual tests */
 
 	/* No-bankswitching */
-	runTest(context, Z_MPCM_TYPE_PCM, sample_2, sizeof(sample_2), 0);
-	runTest(context, Z_MPCM_TYPE_PCM, sample_8, sizeof(sample_8), 0);
-	runTest(context, Z_MPCM_TYPE_PCM, sample_254, sizeof(sample_254), 0);
-	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_2, sizeof(sample_2), 0);
-	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_8, sizeof(sample_8), 0);
-	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_254, sizeof(sample_254), 0);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_2, sample_pcm_2, sizeof(sample_pcm_2), 0);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_8, sample_pcm_8, sizeof(sample_pcm_8), 0);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_254, sample_pcm_254, sizeof(sample_pcm_254), 0);
+	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_pcm_2, sample_pcm_2, sizeof(sample_pcm_2), 0);
+	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_pcm_8, sample_pcm_8, sizeof(sample_pcm_8), 0);
+	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_pcm_254, sample_pcm_254, sizeof(sample_pcm_254), 0);
+	runTest(context, Z_MPCM_TYPE_DPCM, sample_dpcm_4, sample_dpcm_4_output, sizeof(sample_dpcm_4), 0);
+	runTest(context, Z_MPCM_TYPE_DPCM, sample_dpcm_8, sample_dpcm_8_output, sizeof(sample_dpcm_8), 0);
 
-	runTest(context, Z_MPCM_TYPE_PCM, sample_8, sizeof(sample_8), 0x7F00);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_8, sample_pcm_8, sizeof(sample_pcm_8), 0x7F00);
 
 	/* With bankswitching */
-	runTest(context, Z_MPCM_TYPE_PCM, sample_2, sizeof(sample_2), 0x7FFE);
-	runTest(context, Z_MPCM_TYPE_PCM, sample_8, sizeof(sample_8), 0x7FFE);
-	runTest(context, Z_MPCM_TYPE_PCM, sample_254, sizeof(sample_254), 0x7FFE);
-	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_2, sizeof(sample_2), 0x7FFE);
-	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_8, sizeof(sample_8), 0x7FFE);
-	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_254, sizeof(sample_254), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_2, sample_pcm_2, sizeof(sample_pcm_2), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_8, sample_pcm_8, sizeof(sample_pcm_8), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_PCM, sample_pcm_254, sample_pcm_254, sizeof(sample_pcm_254), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_pcm_2, sample_pcm_2, sizeof(sample_pcm_2), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_pcm_8, sample_pcm_8, sizeof(sample_pcm_8), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_PCM_TURBO, sample_pcm_254, sample_pcm_254, sizeof(sample_pcm_254), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_DPCM, sample_dpcm_4, sample_dpcm_4_output, sizeof(sample_dpcm_4), 0x7FFE);
+	runTest(context, Z_MPCM_TYPE_DPCM, sample_dpcm_8, sample_dpcm_8_output, sizeof(sample_dpcm_8), 0x7FFE);
 
 	Z80VM_Destroy(context);
 
