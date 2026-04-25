eb28c508a89251dcf0b4d1cb994c5c37bc404e8d
vladikcomper <rubstov@gmail.com>
2026-04-25 06:06:27 +0200

    Remove some unnecessary files from Git worktree, update .gitignore.

--- lib-68k/update-libs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cp ../../md-modules/build/modules/mdshell/asm68k-linkable/MDShell.obj mdshell.obj
-cp ../../md-modules/build/modules/mdshell/asm68k-linkable/MDShell.asm mdshell.asm
-
-cp ../../md-modules/build/modules/errorhandler/asm68k-linkable/Debugger.obj debugger.obj
-cp ../../md-modules/build/modules/errorhandler/asm68k-linkable/Debugger.asm debugger.asm
